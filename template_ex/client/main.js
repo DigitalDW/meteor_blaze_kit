8cc07b16b0c02c3199f29d5b6a59a89f58199337
Loris Rimaz <loris.rimaz@gmail.com>
2018-02-13 13:50:03 +0100

    Modification de la structure des dossiers
    Comme ça il suffit de modifier une ligne de code pour tester les différents codes d'exemple

---
+++ template_ex/client/main.js
@@ -0,0 +1,1 @@
+import '../imports/ui/body2.js';
---
+++ template_ex/imports/ui/body2.js
@@ -0,0 +1,25 @@
+import { Template } from 'meteor/templating';
+ 
+import './body2.html';
+
+//la fonction "helpers" va être souvent solicitée pour les fonctionnalités de nos templates
+//du coup, on référence nos templates par leur nom
+Template.bonjour.helpers({
+	//on précise quel élément on crée / modifie
+	texte: function(){
+		//ici on met le texte
+		monTexte = "Bonjour, je suis un template"
+		//on le retourne et voilà!
+		return monTexte
+	},
+})
+
+Template.body.helpers({
+	//bien référencer les bons éléments
+	elem: [
+		//utiliser le même nom que dans le HTML: on veut récupérer la valeur "elementListe"
+		{ elementListe: 'element 1' },
+		{ elementListe: 'element 2' },
+		{ elementListe: 'element 3' },
+	],
+});
