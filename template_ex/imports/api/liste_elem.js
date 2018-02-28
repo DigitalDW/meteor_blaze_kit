004c072fb63b7bd6b6bda6812c2fec485317e344
Loris Rimaz <loris.rimaz@gmail.com>
2018-02-28 11:39:22 +0100

    quelques commentaires
    Il rest quelques body#.js à faire…

--- template_ex/imports/api/liste_elem.js
+++ template_ex/imports/api/liste_elem.js
@@ -1,10 +1,14 @@
+//Import des méthodes
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { check } from 'meteor/check';
- 
+
+//Création d'une constante "Elem" qui servira à référencer la BD dans le code
 export const Elem = new Mongo.Collection('listeElem');
 
+//Condition pour le publish
 if (Meteor.isServer) {
+	//Publication des éléments de la BD
   	Meteor.publish('elems', function elemsPublication() {
 	    return Elem.find({
 	      	$or: [
@@ -15,61 +19,83 @@ if (Meteor.isServer) {
 	});
 }
 
+//Création de méthodes pour la BD
 Meteor.methods({
+	//elem.add: prend un String comme argument, ajoute une instance de Elem à la BD
 	'elem.add'(text){
 		check(text, String);
 		Elem.insert({
+			//C'est ici qu'on ajoute le "text" récupéré 
 			elementListe: text,
+			//D'autres éléments créés automatiquement
 			selected: false,
 			createdAt: new Date(),
 			createur: Meteor.userId(),
 			nomUtilisateur: Meteor.user().username,
 		});
 	},
+	//elem.remove: prend un String comme argument -> ici, l'id de l'élément
+	//Supprime l'éléement correspondant à l'id
 	'elem.remove'(elemID){
 		check(elemID, String);
 
+		//Création d'une constante qui contient l'élément en question
 		const elem = Elem.findOne(elemID);
 
+		//Condition: si l'élément est privé et que ce n'est pas le bon utilisateur, faire une erreur
     	if (elem.private && elem.createur !== Meteor.userId()) {
      		throw new Meteor.Error('not-authorized');
     	}
 
 		Elem.remove(elemID);
 	},
+	//elem.editSelect: prend un String et un Boolean comme arguments
+	//Va changer le statut "selected" de d'un élément en fonction de son id
 	'elem.editSelect'(elemID, setSelected){
 		check(elemID, String);
 		check(setSelected, Boolean);
 
+		//Création d'une constante qui contient l'élément en question
 		const elem = Elem.findOne(elemID);
 
+		//Condition: si l'élément est privé et que ce n'est pas le bon utilisateur, faire une erreur
 		if (elem.private && elem.createur !== Meteor.userId()) {
      		throw new Meteor.Error('not-authorized');
     	}
 
 		Elem.update(elemID, { $set: { selected: setSelected } } )
 	},
+	//elem.editName: prend deux Strings comme aruguments
+	//Va changer le nom d'un élément en fonction de son id
 	'elem.editName'(elemID, newName){
 		check(elemID, String);
 		check(newName, String);
-
+		
+		//Création d'une constante qui contient l'élément en question
 		const elem = Elem.findOne(elemID);
 
+		//Condition: si l'élément est privé et que ce n'est pas le bon utilisateur, faire une erreur
 		if (elem.private && elem.createur !== Meteor.userId()) {
      		throw new Meteor.Error('not-authorized');
     	}
 
 		Elem.update(elemID, { $set: { elementListe: newName } } )
 	},
+	//elem.setSelected: ne prend pas d'argument
+	//Méthode statique qui sert à changer le statut "selected" de tous les éléments en "false"
 	'elem.setSelected'(){
 		Elem.update({}, { $set: { selected: false }}, { multi: true } )
 	},
+	//elem.setToPrivate: prend un String et un Boolean comme arguments
+	//Va changer le statut "private" d'un élément en fonction de son id
 	'elem.setToPrivate'(elemID, togglePrivate){
 		check(elemID, String);
 		check(togglePrivate, Boolean);
-
+		
+		//Création d'une constante qui contient l'élément en question
 		const elem = Elem.findOne(elemID);
 
+		//Condition: si l'élément est privé et que ce n'est pas le bon utilisateur, faire une erreur
 		if (elem.createur !== Meteor.userId()){
       		throw new Meteor.Error('not-authorized');
     	}
--- template_ex/imports/ui/body8.js
+++ template_ex/imports/ui/body8.js
@@ -1,9 +1,11 @@
+//Import des méthodes
 import { Template } from 'meteor/templating';
 import { Elem } from '../api/liste_elem.js';
 import { Session } from 'meteor/session';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Meteor } from 'meteor/meteor';
 
+//import du body et des templates
 import './body.html';
 import './template/titre.html';
 import './template/zoneListe.html';
@@ -11,11 +13,13 @@ import './template/liste.html';
 import './template/ajout.html';
 import './template/demande.html';
 
+//Appel des méthodes lors de la création de la page (body)
 Template.body.onCreated(function(){
 	Meteor.call('elem.setSelected');
 	Meteor.subscribe('elems');
 });
 
+//Helpers du body: conditions
 Template.body.helpers({
 	ex1: function(){
 		return false
@@ -37,6 +41,7 @@ Template.body.helpers({
 	}
 });
 
+//Helpers du titre -> affiche le titre
 Template.titre.helpers({
 	text: function(){
 		monTexte = "Les Bases de Données"
@@ -44,16 +49,19 @@ Template.titre.helpers({
 	},
 });
 
+//Helpers de la zone d'affichage des listes -> récupérer les éléments de la BD
 Template.zoneListe.helpers({
 	elem() {
     return Elem.find({});
   },
 });
 
+//Création d'un variable réactive à la création du template "liste"
 Template.liste.onCreated(function(){
 	this.selected = new ReactiveVar( false );
 });
 
+//Helpers de liste -> vérifier si elle est séléctionnée et condition d'affichage
 Template.liste.helpers({
 	selected: function(){
 		return Template.instance().selected.get();
@@ -63,6 +71,7 @@ Template.liste.helpers({
 	}
 });
 
+//Events de liste -> si on clique sur un élément, changé son statut "selected"
 Template.liste.events({
 	'click .liste-li': function(event, template){
 		event.preventDefault();
@@ -75,6 +84,7 @@ Template.liste.events({
 	}
 });
 
+//Helpers de demande -> conditions
 Template.demande.helpers({
   	body8: function(){
   		return true
@@ -84,6 +94,10 @@ Template.demande.helpers({
   },
 });
 
+//Events de demande -> clicks:
+//1) sur #Edit: demander un nouveau nom à l'utilisateur et appeler la méthode "elem.editName"
+//2) sur #delete: appeler la méthode "elem.remove"
+//3) sur #toggle-private": appeler la méthode "elem.setToPrivate"
 Template.demande.events({
 	'click #edit': function(event, template){
 		event.preventDefault();
@@ -104,6 +118,7 @@ Template.demande.events({
 	}
 });
 
+//Events de ajout -> lorsque l'utilisateur submit le formulaire, appeler "elem.add"
 Template.ajout.events({
 	'submit .new-elem': function(event){
 		event.preventDefault();
