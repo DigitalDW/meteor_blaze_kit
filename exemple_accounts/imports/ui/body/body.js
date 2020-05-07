7cb3f75a33a7b4408cb5992c9b1f95b5eefe101a
DigitalDW <loris.rimaz@gmail.com>
2020-05-07 12:23:42 +0200

    Accounts example

---
+++ exemple_accounts/imports/ui/body/body.js
@@ -0,0 +1,36 @@
+import './body.html';
+import '../telechargement/tele.js';
+import '../accueil/accueil.js';
+import '../contact/contact.js';
+import '../login/login.js';
+import '../../../lib/routing.js';
+
+import { Template } from 'meteor/templating';
+import { Meteor } from 'meteor/meteor';
+
+Template.app_body.helpers({
+  utilisateur: () => Meteor.user().username,
+});
+
+Template.app_body.events({
+  'click #accueil'(event) {
+    event.preventDefault();
+    FlowRouter.go('accueil');
+  },
+  'click #tele'(event) {
+    event.preventDefault();
+    FlowRouter.go('tele');
+  },
+  'click #contact'(event) {
+    event.preventDefault();
+    FlowRouter.go('contact');
+  },
+  'click #login'(event) {
+    event.preventDefault();
+    FlowRouter.go('login');
+  },
+  'click #logout'(event) {
+    event.preventDefault();
+    Meteor.logout();
+  },
+});
