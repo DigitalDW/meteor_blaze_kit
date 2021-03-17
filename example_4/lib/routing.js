873e08633c682b0d1789c69c74cfc5fc8df15761
Loris Rimaz <loris.rimaz@gmail.com>
2021-03-17 10:20:41 +0100

    updated examples 3 and 4 (4 of 6)

--- example_4/lib/routing.js
+++ example_4/lib/routing.js
@@ -1,13 +1,15 @@
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
+
 FlowRouter.route('/', {
-    name: 'home',
-    action() {
-        BlazeLayout.render("main");
-    }
+	name: 'home',
+	action() {
+		BlazeLayout.render('main');
+	},
 });
 
 FlowRouter.route('/liste/:id', {
-    name: 'liste',
-    action() {
-        BlazeLayout.render("listeMain");
-    }
+	name: 'liste',
+	action() {
+		BlazeLayout.render('listeMain');
+	},
 });
